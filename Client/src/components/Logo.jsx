b4002f852096282ffd07f02820ba4b999eb00e98
Ahmad N <84658803+Ahmad0222@users.noreply.github.com>
2023-09-25 23:40:51 +0500

    updated code

--- Client/src/components/Logo.jsx
+++ Client/src/components/Logo.jsx
@@ -2,7 +2,7 @@
 import logo from '../assets/images/logo.jpg';
 
 const Logo = () => {
-  return <img src={logo} alt='HUMAN RESOURCES CONSULTANCY' className='logo' style={{width:'60px'}} />;
+  return <img src={logo} alt='HUMAN RESOURCES CONSULTANCY' className='logo' style={{width:'100px', height:"auto", objectFit:"cover"}} />;
 };
 
 export default Logo;
